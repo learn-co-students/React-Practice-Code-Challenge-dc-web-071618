4cf48188757a7b0d5ecad17359bcf8cefa7e02ae
jensnyder <jlsnyder09@gmail.com>
2018-09-24 16:41:09 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,67 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  state = {
+    allSushi: [],
+    idToDisplay: 1,
+    eatenSushi: [],
+    moneyRemaining: 100
+  };
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(sushiData => this.setState({ allSushi: sushiData }));
+  }
+
+  showMoreSushi = () => {
+    if (this.state.idToDisplay > 96) {
+      this.setState({ idToDisplay: 1 });
+    } else {
+      this.setState({ idToDisplay: this.state.idToDisplay + 4 });
+    }
+  };
+
+  eatSushi = sushiId => {
+    let selectedSushi = this.state.allSushi.find(sushi => sushiId === sushi.id);
+    if (
+      selectedSushi.price <= this.state.moneyRemaining &&
+      this.state.eatenSushi.includes(selectedSushi) === false
+    ) {
+      this.setState({
+        eatenSushi: [...this.state.eatenSushi, selectedSushi],
+        moneyRemaining: this.state.moneyRemaining - selectedSushi.price
+      });
+    }
+  };
+
+  addMoney = amount => {
+    this.setState({
+      moneyRemaining: this.state.moneyRemaining + parseInt(amount)
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          allSushi={this.state.allSushi}
+          idToDisplay={this.state.idToDisplay}
+          moneyRemaining={this.state.moneyRemaining}
+          showMoreSushi={this.showMoreSushi}
+          eatSushi={this.eatSushi}
+          eatenSushi={this.state.eatenSushi}
+        />
+        <Table
+          moneyRemaining={this.state.moneyRemaining}
+          eatenSushi={this.state.eatenSushi}
+          addMoney={this.addMoney}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,32 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
+import Wallet from "../components/Wallet";
 
-const Table = (props) => {
-
-  const renderPlates = (array) => {
+const Table = props => {
+  const renderPlates = array => {
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
-    })
-  }
+      return (
+        <div className="empty-plate" key={index} style={{ top: -7 * index }} />
+      );
+    });
+  };
 
   return (
     <Fragment>
+      <Wallet addMoney={props.addMoney} />
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${props.moneyRemaining} remaining!
       </h1>
       <div className="table">
-        <div className="stack">
-          {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
-          }
-        </div>
+        <div className="stack">{renderPlates(props.eatenSushi)}</div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
