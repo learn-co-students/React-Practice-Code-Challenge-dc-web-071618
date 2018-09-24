83fbecb7faee94923cb8e71032db2db6181af584
github username <github email address>
2018-09-24 08:52:39 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,89 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allSushi: [],
+      sushiToRender: [],
+      moneyToSpend: 50,
+      platesEaten: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchSushi();
+  }
+
+  fetchSushi() {
+    fetch("http://localhost:3000/sushis")
+      .then(r => r.json())
+      .then(data =>
+        this.setState({
+          allSushi: data
+        })
+      )
+      .then(() => {
+        this.setState({
+          sushiToRender: [
+            this.state.allSushi[this.getRandomSushi()],
+            this.state.allSushi[this.getRandomSushi()],
+            this.state.allSushi[this.getRandomSushi()],
+            this.state.allSushi[this.getRandomSushi()]
+          ]
+        });
+      });
+  }
+
+  getMoarSushi() {
+    // debugger;
+    this.setState({
+      sushiToRender: [
+        this.state.allSushi[this.getRandomSushi()],
+        this.state.allSushi[this.getRandomSushi()],
+        this.state.allSushi[this.getRandomSushi()],
+        this.state.allSushi[this.getRandomSushi()]
+      ]
+    });
+  }
+
+  getRandomSushi() {
+    return Math.floor(Math.random() * 100 + 1);
+  }
+
+  addPlate(cost) {
+    this.state.moneyToSpend - cost > 0 ? this.addPlateToState(cost) : null;
+  }
+
+  addPlateToState(cost) {
+    // debugger;
+    this.setState({
+      platesEaten: [...this.state.platesEaten, cost]
+    });
+    this.setState({
+      moneyToSpend: this.state.moneyToSpend - cost
+    });
+  }
 
   render() {
+    // debugger;
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          addPlate={this.addPlate.bind(this)}
+          sushiToRender={this.state.sushiToRender}
+          getMoarSushi={this.getMoarSushi.bind(this)}
+          moneyToSpend={this.state.moneyToSpend}
+        />
+        <Table
+          moneyToSpend={this.state.moneyToSpend}
+          platesEaten={this.state.platesEaten}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,26 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from "react";
+import MoreButton from "../components/MoreButton";
+import Sushi from "../components/Sushi";
 
-const SushiContainer = (props) => {
+const SushiContainer = props => {
+  let getMoarSushi = () => {
+    // debugger;
+    props.getMoarSushi();
+  };
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushiToRender.map(sushi => (
+          <Sushi
+            addPlate={props.addPlate}
+            singleSushi={sushi}
+            moneyToSpend={props.moneyToSpend}
+          />
+        ))}
+        <MoreButton getMoarSushi={getMoarSushi} />
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default SushiContainer
+export default SushiContainer;
